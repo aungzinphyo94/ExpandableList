43e537b77a579c20043b30e3c391e9a6c46dcc6f
Aung Zin Phyo <aungzinphyo94@gmail.com>
2019-05-22 03:27:15 +0630

    Add project files.

---
+++ ExpandableList/ExpandableList/Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpandableList
+{
+    class Food
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ ExpandableList/ExpandableList/FoodImageCell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ExpandableList
+{
+    class FoodImageCell : ImageCell
+    {
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            var food = (Food)BindingContext;
+        }
+    }
+}
